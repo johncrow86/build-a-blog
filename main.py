9d1b934fef82b37aab9eed07a5df66150ee4797b
John Crow <johncrow86@hotmail.com>
2016-09-02 12:53:41 -0500

    first commit pages work

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+
+    def renderError(self, error_code):
+        self.error(error_code)
+        self.response.write("Oops! Something went wrong.")
+
+class Postsdb(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+class Blog(Handler):
+    def get(self):
+        blog_posts = db.GqlQuery("SELECT * FROM Postsdb order by created desc limit 5")
+
+        t = jinja_env.get_template("blog.html")
+        response = t.render(posts = blog_posts)
+        self.response.write(response)
+
+class NewPost(Handler):
+    def get(self):
+        t = jinja_env.get_template("newpost.html")
+        response = t.render()
+        self.response.write(response)
+
+    def post(self):
+        post_title = self.request.get("title")
+        post_body = self.request.get("body")
+
+        # check for empty title or body and render same page
+        if (not post_title) or (post_title.strip() == "") or (not post_body) or (post_body.strip() == ""):
+            error = "Please enter a title and a post"
+            t = jinja_env.get_template("newpost.html")
+            response = t.render(title=post_title, body=post_body, error=error)
+            self.response.write(response)
+        # store the post inthe database and redirect home
+        else:
+            p = Postsdb(title = post_title, body = post_body)
+            p.put()
+            self.redirect("/")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', Blog),
+    ('/newpost', NewPost)
 ], debug=True)
